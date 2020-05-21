6cbe97858c8edf0c8ae2a21420ecdce322a4318a
arielmerin <arielmerino@ciencias.unam.mx>
2020-05-20 19:51:59 -0500

    Añadiendo serializable
    NO se ha podido hacer que el objeto en cuestión se haga serializable, se seguiran haciendo pruebas y enviando correo a los profes

--- src/Main.java
+++ src/Main.java
@@ -1,33 +1,35 @@
 import dict.Diccionario;
+import serializer.Serializer;
 import util.ArbolAVL;
 import util.Coleccionable;
 import util.Lista;
 
+import java.io.File;
+import java.util.logging.FileHandler;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello muuuuuuuundo");
-
         Diccionario diccionario = new Diccionario();
         Lista<String> lista = diccionario.leeTXT();
-        ArbolAVL<String> stringArbolAVL = new ArbolAVL<>(lista);
-        //System.out.println(stringArbolAVL);
-        Lista<String> palabrasCambias = diccionario.cambiaPalabra("valjena");
+
+        File archivo = new File("dataD.dat");
+        Serializer serializer = new Serializer();
+
+        ArbolAVL<String> stringArbolAVL;
+        stringArbolAVL = new ArbolAVL<>(lista);
+
+        serializer.write(stringArbolAVL, "dataD.ser");
+
+        System.out.println(stringArbolAVL);
+        Lista<String> palabrasCambias = diccionario.cambiaPalabra("vallena");
         System.out.println(palabrasCambias);
-        Lista<Lista<String>> doblePalabraCambiada  = new Lista<>();
-        for ( String palabra: palabrasCambias ){
-            doblePalabraCambiada.agregar(diccionario.cambiaPalabra(palabra));
-        }
-        System.out.println(doblePalabraCambiada);
-
-
-        for (Lista<String> lista1: doblePalabraCambiada){
-            for (String palabra1: lista1){
-                if (stringArbolAVL.contiene(palabra1)){
-                    ArbolAVL.NodoAVL nodoAVL = (ArbolAVL.NodoAVL) stringArbolAVL.buscando(palabra1);
-                    System.out.println(nodoAVL);
-                    System.out.println(nodoAVL.izquierdo);
-                    System.out.println(nodoAVL.derecho);
-                }
+        for (String palabra: palabrasCambias){
+            if (stringArbolAVL.contiene(palabra)){
+                ArbolAVL.NodoAVL nodoAVL = (ArbolAVL.NodoAVL) stringArbolAVL.buscando(palabra);
+                System.out.println(nodoAVL);
+                System.out.println(nodoAVL.izquierdo);
+                System.out.println(nodoAVL.derecho);
             }
         }
     }
